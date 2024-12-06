52bf9d3f36499dc12ff62a49389bc4620fc64ca2
JoseArgz8658 <jose.miguel11.bkn@gmail.com>
2024-12-06 18:21:11 -0300

    update

--- BackEnd/main/views.py
+++ BackEnd/main/views.py
@@ -3,6 +3,8 @@ from django_user_agents.utils import get_user_agent
 
 def index(request):
     user_agent = get_user_agent(request)
-    apk_url = request.build_absolute_uri('/static/apk/IonicCraft.apk')  # URL completa al APK
+    
+    apk_url = request.build_absolute_uri('/static/apk/IonicCraft.apk')
+    
     context = {'mobile': user_agent.is_mobile, 'apk_url': apk_url}
     return render(request, 'index.html', context)
